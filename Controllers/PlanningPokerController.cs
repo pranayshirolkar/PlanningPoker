ed4696479fd56e4e06fe6b63e3478ea4be1adba4
Pranay Shirolkar <pshirolkar@servicetitan.com>
2020-06-02 22:12:57 -0700

    Adds input parsing based on regex matches

--- Controllers/PlanningPokerController.cs
+++ Controllers/PlanningPokerController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -17,7 +18,7 @@ namespace PlanningPoker.Controllers
         {
             this.votesRepository = votesRepository;
         }
-        
+
         [Route("[action]")]
         [HttpPost]
         public async Task<IActionResult> PokerInteract([FromForm] string payload)
@@ -37,7 +38,7 @@ namespace PlanningPoker.Controllers
                 if (wasVoteAdded)
                 {
                     var responseMessage = MessageHelpers.GetMessageWithNewVoteAdded(p.Message.Blocks, p.User.Username);
-                    await responseMessage.Send(p.ResponseUrl);    
+                    await responseMessage.Send(p.ResponseUrl);
                 }
             }
 
@@ -55,19 +56,57 @@ namespace PlanningPoker.Controllers
                 dataset[i] = key + "=" + value;
                 i++;
             }
+
             var payload = string.Join('&', dataset);
             var slashCommand = new SlashCommand(payload);
 
             var arguments = slashCommand.Text.Split(" ");
             if (arguments.Length == 1 && string.IsNullOrEmpty(arguments[0]))
             {
-                var m = MessageHelpers.CreateEphemeralMessage("Please add what to deal `/poker [what] [who]`. Adding who is optional.");
+                var m = MessageHelpers.CreateEphemeralMessage(
+                    "Please add what to deal `/poker [what]`. Optionally, to get grouped results, add groups: `/poker [@group1] [@group2] [what]`");
                 await m.Send(slashCommand.ResponseUrl);
             }
             else
             {
-                var message = MessageHelpers.CreateDealtMessage(slashCommand.Username, slashCommand.Text);
-                await message.Send(slashCommand.ResponseUrl);   
+                var users = "";
+                var whatToDeal = "";
+                var x = 0;
+                while (x < arguments.Length)
+                {
+                    if (Regex.Match(arguments[x], @"<@U.*>").Success)
+                    {
+                        var m = MessageHelpers.CreateEphemeralMessage(
+                            "Looks like a `@user` was tagged instead of a `@userGroup`. It will be ignored.");
+                        await m.Send(slashCommand.ResponseUrl);
+                    }
+
+                    if (Regex.Match(arguments[x], @"<!subteam\^.*>").Success)
+                    {
+                        //todo check if valid user group else throw
+                        users += (arguments[x]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    x++;
+                }
+
+                whatToDeal = string.Join(' ', arguments.Skip(x));
+
+                if (string.IsNullOrEmpty(whatToDeal))
+                {
+                    var m = MessageHelpers.CreateEphemeralMessage(
+                        "Please add what to deal `/poker [@group1] [@group2] [what]`");
+                    await m.Send(slashCommand.ResponseUrl);
+                }
+                else
+                {
+                    var message = MessageHelpers.CreateDealtMessage(slashCommand.Username, whatToDeal);
+                    await message.Send(slashCommand.ResponseUrl);
+                }
             }
 
             return Ok();
